1016384bc0af4ad49d3447123a3f23d43f054b48
CSharpProjector <marleymulvinbroome@gmail.com>
2022-12-01 19:53:31 +0900

    Styled glossary page

--- scripts/glossary.js
+++ scripts/glossary.js
@@ -4,6 +4,36 @@
     { jp: }
 ];*/
 
-fetch("../test.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
+const toggleLoad = () => {
+    const loader = $('.loader');
+
+    if (loader.length) {
+        loader.remove();
+    } else {
+        $('.glossary-entries').add('div').addClass('loader');
+    }
+}
+
+const addEntry = (entry) => {
+
+}
+
+const loadGlossary = (json) => {
+    console.log("Loading glossary title: " + json.glossary.title);
+
+    for (let i in json.glossary.entries) {
+        addEntry(json.glossary.entries[i]);
+    }
+
+    //toggleLoad();
+
+}
+
+$(document).ready(function(){
+    //toggleLoad();
+    fetch("../data/glossary.json")
+    .then(response => response.json())
+    .then(json => loadGlossary(json));
+});
+
+
