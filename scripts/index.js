745131edf53cf48df02e4eb123263cc572b1664f
CSharpProjector <marleymulvinbroome@gmail.com>
2022-12-01 18:22:07 +0900

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,2 @@
+const ytRequest = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCIsciPYL8OkM6asz6Xr1JMXbrAhUUppV4&channelId=UCZKajgue-U1mUdwqF2WSUqw&part=snippet,id&order=date&maxResults=1"
+
